b1c7c93e9e8a3816948297cab619e77dce5918eb
Stephen Röttger <stephen.roettger@zero-entropy.de>
2013-03-27 14:05:53 +0100

    moved fpprotect from external lib to libc

---
+++ fpprotect/fpprotect.h
@@ -0,0 +1,10 @@
+//
+//#ifndef _FPPROTECT_H
+//#define _FPPROTECT_H 1
+//
+//typedef void *unprotected_t __attribute__((fpprotect_disable));
+//extern void *fpp_protect_func_ptr (unprotected_t);
+//
+//#define FPP_UNPROTECTED(ptr) (void (*)() __attribute__((fpprotect_disable))) (ptr)
+//
+//#endif /* fpprotect.h */
